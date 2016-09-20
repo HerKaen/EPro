adad37be159d6480ba41567e48bd1d55d70390c8
HerKaen <nicoglenz@arcor.de>
2016-09-20 12:57:18 +0200

    Aktualisiert  ruby

--- Ruby2/Unterricht/start.rb
+++ Ruby2/Unterricht/start.rb
@@ -19,3 +19,12 @@
 
 # --------------------------------------------------
 
+def movie_listing(movie, rank)
+	"Movie: #{movie} ist ein guter Film, mit der Punktzahl: #{rank} am #{set_time}"
+end
+
+def set_time
+	Time.now.strftime("%d.%m.%Y")
+end
+
+puts movie_listing(ARGV[0], 10) #Erster Wert wird bei Abfrage eingegeben (ruby start.rb Superman)
