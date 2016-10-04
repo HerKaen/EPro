6567140112ec722f1c95a46d6662a11f233a04e6
HerKaen <nicoglenz@arcor.de>
2016-10-04 12:49:57 +0200

    array

--- Ruby2/Unterricht/hello.rb
+++ Ruby2/Unterricht/hello.rb
@@ -1,6 +1,3 @@
-#hello = "Schönen guten tag!"
-#puts "Schönen guten tag!"
-
 class Movie
 
 	attr_reader :title
@@ -30,8 +27,21 @@ class Movie
 	end
 end
 
-movie = Movie.new("Goonies", 10)
-puts movie.title
-movie.title = "GYnniies"
-puts movie.title
-puts movie.normalized_rank
+movie1 = Movie.new("Goonies", 10)
+movie2 = Movie.new("Batman", 5)
+movie3 = Movie.new("Spiderman", 8)
+
+movies = [movie1, movie2, movie3]
+
+	movies.each do |movie|
+ 	movie.thumbs_down
+	puts movie
+	end
+
+
+
+#movie = Movie.new("Goonies", 10)
+#puts movie.title
+#movie.title = "GYnniies"
+#puts movie.title
+#puts movie.normalized_rank
--- Ruby2/Unterricht/start.rb
+++ Ruby2/Unterricht/start.rb
@@ -31,27 +31,99 @@
 
 # ---------------------------------------------------
 
-class Movie
-	def initialize(pname, prank=5)
-		@name = pname
-		@rank = prank
+#class Movie
+#	def initialize(pname, prank=5)
+#		@name = pname
+#		@rank = prank
+#	end
+
+#	def list_movie
+#		"Movie heißt: #{@name} und hat den Rank #{@rank}."
+#	end
+
+#	def thumbs_up(value)
+#		@rank = @rank + value
+#	end
+
+
+#end
+
+#var = Movie.new("Superman")
+#puts var.list_movie 
+#var.thumbs_up(7)
+#puts var.list_movie
+
+#var2 = Movie.new("Batman", 10)
+#puts var2.list_movie
+
+#---------------------------------------------------------------------
+
+#class Customer
+		#1 #def set_name(name)
+		#1 #	@name = name
+		#1 #end
+
+		#2 #def name=(name)
+		#2 #	@name = name
+		#2 #end
+
+#		attr_writer :name   #3
+
+#		def ausgabe
+#			@name
+#		end
+#	end
+
+#	obj = Customer.new
+	#1 #obj.set_name("Theo")
+
+#	obj.name = "Theo"   #2 + 3
+#	puts obj.ausgabe
+
+#-----------------------------------------------------------------------
+
+class Playlist
+	def initialize(name)
+		@name = name
+		@movies = []
 	end
 
-	def list_movie
-		"Movie heißt: #{@name} und hat den Rank #{@rank}."
+	def add_movie(movie)
+		@movies << movie
 	end
 
-	def thumbs_up(value)
-		@rank = @rank + value
+	def play
+		puts "#{@name} Playlist:"
+		@movies.each do |movie|
+			puts movie
 	end
-
-
+end
 end
 
-var = Movie.new("Superman")
-#puts var.list_movie 
-var.thumbs_up(7)
-puts var.list_movie
+class Movie
 
-var2 = Movie.new("Batman", 10)
-puts var2.list_movie
+	attr_accessor :title #reader & writer in einem!
+
+	def initialize(title, rank=0)
+		@title = title.capitalize
+		@rank = rank
+	end
+
+	def thumbs_down
+		@rank -= 1
+	end
+
+	def to_s
+		"#{@title} has a rank of #{@rank}."
+	end
+end
+
+movie1 = Movie.new("Goonies", 10)
+movie2 = Movie.new("Batman", 5)
+movie3 = Movie.new("Spiderman", 8)
+
+
+playlist1 = Playlist.new("Theo")
+playlist1.add_movie(movie1)
+playlist1.add_movie(movie2)
+playlist1.play
