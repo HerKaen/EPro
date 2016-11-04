41ddc4fb81305666780f7d25e789b5ce3bc448a8
HerKaen <nicoglenz@arcor.de>
2016-11-04 14:39:36 +0100

    learning

--- Ruby2/Unterricht/ueben3.rb
+++ Ruby2/Unterricht/ueben3.rb
@@ -1,33 +1,12 @@
-Person1 = {"Vorname" => "Theo", "Nachname" => "Sommer", "Stadt" => "Lodz"}
-Person2 = {"Vorname" => "Stevie", "Nachname" => "K", "Stadt" => "Kraselhausen"}
-Person3 = {"Vorname" => "Garry", "Nachname" => "Ewert", "Stadt" => "Schwuchtelstadt"}
-
-Adresse = Array.new
-Person1.each do |val|
-Adresse << val
+str_1 = "Programmieren, Federball, Whisky, Bier"
+str_2 = "Fussball, Bier, Programmieren"
+str1 = str_1.scan(/\w+/)
+str2 = str_2.scan(/\w+/)
+str1.size.times do |i|
+	if (str2.include? str1[i]) == true
+		diff = (str1 - str2) + (str2 - str1)
+		"ergebnis = { :diff #{diff}, :match [\"#{str1[i]}\"] }"
+		#ergebnis = { :diff => diff, :match => str1[i] }
+		puts "ergebnis = #{ergebnis}"
 end
-
-Person2.each do |val2|
-Adresse << val2
 end
-
-Person3.each do |val3|
-Adresse << val3
-end
-
-puts "1.Person:"
-Adresse[0..2].each do |key, val|
-puts "#{key}: #{val}"
-end 
-
-puts "2.Person:"
-Adresse[3..5].each do |key, val|
-puts "#{key}: #{val}"
-end
-
-puts "3.Person:"
-Adresse[6..8].each do |key, val|
-puts "#{key}: #{val}"
-end
-
-
