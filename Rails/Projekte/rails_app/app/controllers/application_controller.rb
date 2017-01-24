094cc80fcefefc3a1192f98da8440bfe0b528278
HerKaen <nicoglenz@arcor.de>
2017-01-24 12:50:33 +0100

    Sessions

--- Rails/Projekte/rails_app/app/controllers/application_controller.rb
+++ Rails/Projekte/rails_app/app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-before_action :set_locale
+  def current_user
+  	@current_user = User.find(session[:user_id]) if session[:user_id]
+  end
+
+
+  helper_method :current_user
+
+
+  before_action :set_locale
 
 	def set_locale
 		I18n.locale = I18n.default_locale
 	end
 end
+ 
