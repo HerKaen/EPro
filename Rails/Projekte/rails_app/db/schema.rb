8d65261c1de2d1c90c48957a2bcc1da0cb8139b1
HerKaen <nicoglenz@arcor.de>
2017-01-31 12:05:15 +0100

    Krueger2

--- Rails/Projekte/rails_app/db/schema.rb
+++ Rails/Projekte/rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124075456) do
+ActiveRecord::Schema.define(version: 20170131092016) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170124075456) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "admin"
   end
 
 end
